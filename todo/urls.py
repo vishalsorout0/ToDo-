d5d98ab780865f339e8c6266c1460b992a34f0fb
vishalsorout0 <sorotvishal9@gmail.com>
2026-01-17 23:42:19 +0530

    todo final

--- todo/urls.py
+++ todo/urls.py
@@ -5,4 +5,6 @@ urlpatterns=[
     path('addtask/',views.addtask,name='addtask'),
     path('mark_as_done/<int:pk>',views.mark_as_done,name='mark_as_done'),
     path('undone/<int:pk>',views.undone,name='undone'),
+    path('edit_task/<int:pk>/',views.edit_task,name='edit_task'),
+    path('delete_task/<int:pk>/',views.delete_task,name='delete_task'),c
 ]
--- todo/views.py
+++ todo/views.py
@@ -20,4 +20,22 @@ def undone(request,pk):
     task.save()
     return redirect('home')
 
+def edit_task(request,pk):
+    get_task=get_object_or_404(Task,pk=pk)
+    if request.method=='POST':
+        new_task=request.POST['task'] 
+        get_task.task=new_task
+        get_task.save()
+        return redirect('home')
+    else:    
+        context={
+            'get_task': get_task,
+        }
+        return render(request,'edit_task.html',context)
+    
+def delete_task(request,pk):
+    task=get_object_or_404(Task,pk=pk)
+    task.delete()
+    return redirect('home')
+
 
